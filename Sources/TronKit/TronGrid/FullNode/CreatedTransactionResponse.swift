d14c58ed48863d50828d93e6325d4336024e1e71
Anton Stavnichiy <anton.stavnichiy@gmail.com>
2026-01-18 16:52:56 +0700

    Fix broadcast transaction

--- Sources/TronKit/TronGrid/FullNode/CreatedTransactionResponse.swift
+++ Sources/TronKit/TronGrid/FullNode/CreatedTransactionResponse.swift
@@ -5,12 +5,14 @@ public struct CreatedTransactionResponse: ImmutableMappable {
     let visible: Bool?
     let txID: Data
     let rawData: TransactionResponse.RawData
+    let rawDataMap: [String: Any]
     let rawDataHex: Data
 
     public init(map: Map) throws {
         visible = (try? map.value("visible")) ?? false
         txID = try map.value("txID", using: HexDataTransform())
         rawData = try map.value("raw_data")
+        rawDataMap = try map.value("raw_data")
         rawDataHex = try map.value("raw_data_hex", using: HexDataTransform())
     }
 }
